0deed746280de577364cdbb05b86e58ac37930ac
singhanand6510 <singhanand6510@yahoo.com>
2024-12-06 06:26:22 +0530

    basic mongoose connection

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose"
 import { driver, createAstraUri } from "stargate-mongoose"
 
 export const connectToAstraDb = async (): Promise<void> => {
-  const endpoint = process.env.ASTRA_DB_API_ENDPOINT
-  const token = process.env.ASTRA_DB_APPLICATION_TOKEN
+  const endpoint = process.env.ASTRA_DB_API_ENDPOINT!
+  const token = process.env.ASTRA_DB_APPLICATION_TOKEN!
 
   if (!endpoint || !token) {
     throw new Error("Astra DB API endpoint or application token is missing in environment variables.")
